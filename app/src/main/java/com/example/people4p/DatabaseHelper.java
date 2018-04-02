99249ca139604f149ee7f4abd31631da59a3e552
shagun11 <shagun11dhingra@gmail.com>
2018-04-02 01:33:28 -0500

    Integrated SQLite

---
+++ app/src/main/java/com/example/people4p/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.people4p;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "P4p.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table user (name text, email text primary key, mobile text, password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS user");
+        onCreate(db);
+    }
+
+    // inserting into database
+    public boolean insert(String name, String email, String mobile, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name", name);
+        cv.put("email", email);
+        cv.put("mobile", mobile);
+        cv.put("password", password);
+        long ins = db.insert("user", null, cv);
+        if(ins == -1) return false;
+        else return true;
+    }
+
+    //checking if email already exists
+    public boolean checkEmail(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from user where email=?", new String[]{email});
+        if(cursor.getCount() > 0) return false;
+        else return true;
+    }
+}
