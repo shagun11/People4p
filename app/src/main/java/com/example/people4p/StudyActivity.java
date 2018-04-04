963c7c082884eba6bb4f422b8b54963b6c0853d8
Neil <neilio34@gmail.com>
2018-04-03 23:43:49 -0500

    Merge branch 'dev' into dev-Neil
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	build.gradle

--- app/src/main/java/com/example/people4p/StudyActivity.java
+++ app/src/main/java/com/example/people4p/StudyActivity.java
@@ -4,26 +4,29 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import java.util.Calendar;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class StudyActivity extends AppCompatActivity {
 
-    private CountDownTimer timer;
     private TextView clockText;
+    private TextView timeDoneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
         clockText = (TextView) findViewById(R.id.timer);
+        timeDoneText = (TextView) findViewById(R.id.timeDone);
         startTimer();
     }
 
     private void startTimer() {
         clockText.setText("00:00");
 
-        timer = new CountDownTimer(10 * 1000 * 60, 1000) {
+        CountDownTimer timer = new CountDownTimer(10 * 1000 * 60, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -57,6 +60,14 @@ public class StudyActivity extends AppCompatActivity {
         };
 
         timer.start();
+
+        int hourInt = Calendar.HOUR;
+        int minuteInt = Calendar.MINUTE;
+
+        String doneAt = "Done at: " + Integer.toString(hourInt) + ":" + Integer.toString(minuteInt + 10);
+
+        timeDoneText.setText(doneAt);
+
 
     }
 
