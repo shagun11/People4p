c746f28a333d822f4eb69e591377c5f13e10c7a6
Weixx433 <weixx433@umn.edu>
2018-04-08 23:21:25 -0500

    update the profile screen and calendar screen

--- app/src/main/java/com/example/people4p/MainActivity.java
+++ app/src/main/java/com/example/people4p/MainActivity.java
@@ -1,21 +1,48 @@
 package com.example.people4p;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        prefs = getSharedPreferences("MY_DATA", MODE_PRIVATE);
+
+        String name = prefs.getString("MY_NAME", "no user name");
+        String school = prefs.getString("MY_SCHOOL", "no school name");
+        String major = prefs.getString("MY_MAJOR", "no major");
+        String email = prefs.getString("MY_EMAIL", "no email");
+        int phonenumber = prefs.getInt("MY_PHONE", 0);
+
+        ((TextView)findViewById(R.id.user_name)).setText(name);
+        ((TextView)findViewById(R.id.user_school)).setText(school);
+        ((TextView)findViewById(R.id.user_major)).setText(major);
+        ((TextView)findViewById(R.id.user_email)).setText(email);
+        ((TextView)findViewById(R.id.user_phonenumber)).setText(phonenumber+"");
+
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
     }
+
+    public void editProfile(View view) {
+        startActivity(new Intent(getApplicationContext(), ProfileEdit.class));
+//        Intent intent = new Intent(this, ProfileEdit.class);
+//        startActivity(intent);
+    }
+
 }
 
