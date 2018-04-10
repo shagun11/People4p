53efecb2720b47a9eb5d9f44880fe09091666d91
Weixx433 <weixx433@umn.edu>
2018-04-10 00:58:18 -0500

    Add back buttons on both profile screen and calendar screen that can make users to get back to the main screen

--- app/src/main/java/com/example/people4p/calendarActivity.java
+++ app/src/main/java/com/example/people4p/calendarActivity.java
@@ -1,7 +1,9 @@
 package com.example.people4p;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class calendarActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class calendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+    }
+
+
+    public void getBack(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
