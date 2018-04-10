d8e8b00a9c85deb137d6d9467f19435c30201ffa
shagun11 <shagun11dhingra@gmail.com>
2018-04-10 01:08:19 -0500

    Added task functionality

--- app/src/main/java/com/example/people4p/MainActivity.java
+++ app/src/main/java/com/example/people4p/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.people4p;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -90,6 +88,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void activityTask(View view) {
+        Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+        startActivity(intent);
     }
 }
 
